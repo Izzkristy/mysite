939b0ed5a4e8c69a87b70980c96555be46318988
zhangze <zhangze@xiaonongnv.com>
2015-10-29 23:54:53 +0800

    index

--- index/views.py
+++ index/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+
+class IndexView(View):
+   def get(self, request, *args, **kwargs):
+        return JsonResponse({'code':0})
 
 # Create your views here.
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from index.views import  IndexView
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+     url(r'^$',IndexView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
