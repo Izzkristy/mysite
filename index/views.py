73a1974e0d441f5a347b742f9d033e965e04adcc
zhangze <zhangze0722@gmail.com>
2015-11-01 22:29:58 +0800

    hello kristy templates

--- index/views.py
+++ index/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import JsonResponse
+from django.template.response import TemplateResponse
 
 class IndexView(View):
    def get(self, request, *args, **kwargs):
-        return JsonResponse({'code':0})
+        return TemplateResponse(request, 'index.html', {})
 
 # Create your views here.
