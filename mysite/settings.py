7739b7734646acaa959e81cae9f8a9072312211a
zhangze <zhangze@xiaonongnv.com>
2016-03-12 14:17:33 +0800

    mysql setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,8 +59,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
+        'NAME': 'krsity',
+        'USER': 'krsity',
+        'PASSWORD': 'kristy_zz',
+        'OPTIONS': {
+            'charset': 'utf8',
+        },
     }
 }
 
